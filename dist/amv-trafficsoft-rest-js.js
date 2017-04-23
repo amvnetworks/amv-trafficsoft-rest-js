dd8fd11af01de00a34d57f215dcea916d78d0efa
Alois Leitner <alois.leitner@amv-networks.com>
2017-04-23 09:52:33 +0200

    update tests

--- dist/amv-trafficsoft-rest-js.js
+++ dist/amv-trafficsoft-rest-js.js
@@ -18962,9 +18962,12 @@ var xfcdClient = function xfcdClient(baseUrl, options) {
     baseURL: baseUrl + '/xfcd'
   }, options);
 
+  console.log(xfcdRequestOptions.baseURL);
+
   var httpClient = _axios2.default.create(xfcdRequestOptions);
   httpClient.defaults.timeout = 45000;
   httpClient.defaults.headers.post['Content-Type'] = 'application/json';
+  httpClient.defaults.headers.post['User-Agent'] = 'amv-trafficsoft-rest-js/1.0.0';
 
   var getLastData = function getLastData(vehicleIdsArray, options) {
     var url = '/last';
--- test/server/amv-trafficsoft-rest-js.spec.js
+++ test/server/amv-trafficsoft-rest-js.spec.js
@@ -2,7 +2,96 @@ import amvTrafficsoftRestJs from '../../src/amv-trafficsoft-rest-js.js';
 import nock from 'nock';
 
 describe('amvTrafficsoftRestJs', () => {
+  var BASE_URL;
+  var SERVER_MOCK;
+  var DEFAULT_OPTIONS;
+
   before(() => {
+    var mockResponseGetData = [{
+      "deliveryId": 0,
+      "timestamp": "2017-04-23T07:40:49.572Z",
+      "track": [
+        {
+          "id": 0,
+          "nodes": [
+            {
+              "altitude": 0,
+              "hdop": 0,
+              "heading": 0,
+              "id": 0,
+              "latitude": 0,
+              "longitude": 0,
+              "satellites": 0,
+              "speed": 0,
+              "states": [
+                {
+                  "latitude": 0,
+                  "longitude": 0,
+                  "param": "string",
+                  "timestamp": "2017-04-23T07:40:49.572Z",
+                  "value": "string"
+                }
+              ],
+              "timestamp": "2017-04-23T07:40:49.572Z",
+              "vdop": 0,
+              "xfcds": [
+                {
+                  "latitude": 0,
+                  "longitude": 0,
+                  "param": "string",
+                  "timestamp": "2017-04-23T07:40:49.572Z",
+                  "value": "string"
+                }
+              ]
+            }
+          ],
+          "vehicleId": 0
+        }
+      ]
+    }
+  ];
+    var mockResponseGetLastData = [{
+       "altitude": 0,
+       "hdop": 0,
+       "heading": 0,
+       "id": 0,
+       "latitude": 0,
+       "longitude": 0,
+       "satellites": 0,
+       "speed": 0,
+       "states": [
+         {
+           "latitude": 0,
+           "longitude": 0,
+           "param": "string",
+           "timestamp": "2017-04-23T07:40:49.580Z",
+           "value": "string"
+         }
+       ],
+       "timestamp": "2017-04-23T07:40:49.580Z",
+       "vdop": 0,
+       "xfcds": [
+         {
+           "latitude": 0,
+           "longitude": 0,
+           "param": "string",
+           "timestamp": "2017-04-23T07:40:49.580Z",
+           "value": "string"
+         }
+       ]
+      }
+    ];
+
+    BASE_URL = 'http://www.example.com';
+    SERVER_MOCK = nock(BASE_URL)
+         .post('/1/xfcd/last')
+         .reply(200, mockResponseGetLastData);
+
+    DEFAULT_OPTIONS = {
+      username: 'john_doe',
+      password: 'password',
+      contractId: 1
+    };
 
   });
 
@@ -37,24 +126,8 @@ describe('amvTrafficsoftRestJs', () => {
     expect(xfcdClient).to.be.ok;
   });
 
-
-
   it('should request last xfcd data', (done) => {
-    var baseUrl = 'http://www.example.com';
-    var server = nock(baseUrl)
-         .post('/1/xfcd/last')
-         .reply(200, {
-           username: 'pgte',
-           email: 'pedro.teixeira@gmail.com',
-           _id: '4324243fsd'
-         });
-
-    var options = {
-      username: 'john_doe',
-      password: 'password',
-      contractId: 1
-    };
-    var clientFactory = amvTrafficsoftRestJs(baseUrl, options);
+    var clientFactory = amvTrafficsoftRestJs(BASE_URL, DEFAULT_OPTIONS);
 
     var xfcdClient = clientFactory.xfcd();
 
@@ -63,7 +136,6 @@ describe('amvTrafficsoftRestJs', () => {
     var vehicleIds = [1,2,3];
     xfcdClient.getLastData(vehicleIds)
       .then(response => {
-
         console.log('ok - got data');
 
         expect(response).to.be.ok;
@@ -71,9 +143,6 @@ describe('amvTrafficsoftRestJs', () => {
         done();
       })
       .catch(e => {
-        //console.log(e);
-        //console.log('fuck - got NO data');
-        //done();
         done(e);
       });
   });
