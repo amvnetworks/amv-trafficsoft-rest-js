552055686be9762783f83b363173e984c3b8633f
theborakompanioni <theborakompanioni+github@gmail.com>
2018-06-27 02:41:55 +0200

    add bowser test with src as file source

---
+++ test/client/amv-trafficsoft-rest-js-src.spec.js
@@ -0,0 +1,7 @@
+import amvTrafficsoftRestJs from "../../src/amv-trafficsoft-rest-js";
+
+describe('amvTrafficsoftRestJs', () => {
+  it('should be running without any problems', () => {
+    expect(amvTrafficsoftRestJs).to.not.throw();
+  });
+});
